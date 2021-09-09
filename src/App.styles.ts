030c7728ba4a6df76cee91e17c66678c26bb87d6
matthewogtong <ogtongm@gmail.com>
2021-09-08 21:32:50 -0400

    Import IconButton and add to StyledButton

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
+import IconButton from '@material-ui/core/IconButton'
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    margin: 40px;
+`;
+
+export const StyledButton = styled(IconButton)
